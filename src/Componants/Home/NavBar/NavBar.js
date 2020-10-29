b947bba9601f0b18dcc0acf6b70f4d713ceb30d6
Abir-Hasan-GUB <abirhasan6111@gmail.com>
2020-10-29 08:46:20 +0600

    nav bar done

--- src/Componants/Home/NavBar/NavBar.js
+++ src/Componants/Home/NavBar/NavBar.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import './NavBar.css';
 
 const NavBar = () => {
     return (
-        <div>
-            <h1>NavBar</h1>
-        </div>
+        <header id="home">
+            <nav class="navbar navbar-dark navbar-expand-lg hasan">
+            <div class="container">
+                <a href="index.html" class="navbar-brand abir">Abir Hasan</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <div class="menu-item  ml-auto">
+                        <ul class="navbar-nav" id="scroll-active">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#home">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#about">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#skills">Skills</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#service">Services</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#portfolio">Portfolio</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#contact">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        </header>
     );
 };
 
